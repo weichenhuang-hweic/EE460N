b803cf9818fd1499f70cfa48400bedf2fb691f8a
weichen.huang <weichen.huang.hweic@gmail.com>
2024-09-08 22:01:29 -0500

    feat: [Lab1] update testcase file

--- lab1/test.asm
+++ lab1/test.asm
@@ -1,11 +1,27 @@
-; Test Assembly Code
+;This program counts from 10 to 0
+	.ORIG x3000   	
+START	ADD R1, R1, #-1
+	BRZ DONE
+	BR START
+    JSR JP
+    AND R1, R1, #0
+JP  ADD R1, R1, #-1
+	LDB R4, R2, #-10
+	LDW R3,R3,#0
+	LEA R5, EEA
+	NOP
+	RET
+	LSHF R2, R3, #3 ; R2 ê LSHF(R3, #3)
+	RSHFL R2, R3, #7 ; R2 ê RSHF(R3, #7, 0)
+	RSHFA R2, R3, #7 ; R2 ê RSHF(R3, #7, R3[15])
+	STB R4, R2, #10 
+	STW R4, R2, #10 
+DONE	TRAP x25		;The last executable instruction
+	HALT
+TEN	.FILL x000A		;This is 10 in 2's comp, hexadecimal
+EEA .FILL x5000
+	.END			;The pseudo-op, delimiting the source program
 
-    .ORIG x4000
-    AND R0, R0, #0
-    ADD R0, R0, #4
-    LEA R1, A
-    LDW R1, R1, #0
-    STW R0, R1, #0
-    HALT
-A   .FILL x3000	
-    .END
+EEA .FILL x5000
+EEA .FILL x5000
+EEA .FILL x5000
