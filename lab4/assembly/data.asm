51104f7dbc962afe8cd9ce48eff9b11b78bcbd45
weichen.huang <weichen.huang.hweic@gmail.com>
2024-10-14 16:30:43 -0500

    feat: [Lab4][ASM] Finish data.asm

---
+++ lab4/assembly/data.asm
@@ -0,0 +1,24 @@
+.ORIG   xC000
+
+.FILL   x12
+.FILL   x11
+.FILL   x39
+.FILL   x23
+.FILL   x02
+.FILL   xF6
+.FILL   x12
+.FILL   x23
+.FILL   x56
+.FILL   x89
+.FILL   xBC
+.FILL   xEF
+.FILL   x00
+.FILL   x01
+.FILL   x02
+.FILL   x03
+.FILL   x04
+.FILL   x05
+.FILL   x06
+.FILL   x07
+
+.END
